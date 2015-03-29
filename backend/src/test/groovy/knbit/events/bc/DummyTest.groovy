6ee3216d690d9764aa92b5a5b200e03346e85ab2
novy154 <slnowak154@gmail.com>
2015-03-29 15:39:08 +0200

    [BEBC-30] Added spock

---
+++ backend/src/test/groovy/knbit/events/bc/DummyTest.groovy
@@ -0,0 +1,15 @@
+package knbit.events.bc
+
+import spock.lang.Specification
+
+/**
+ * Created by novy on 29.03.15.
+ */
+class DummyTest extends Specification {
+
+    def "should always pass"() {
+        expect:
+        true
+
+    }
+}
