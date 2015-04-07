4521ca54376f8dc219cc4bb72cec29f2c5e1c636
novy154 <slnowak154@gmail.com>
2015-04-07 12:40:52 +0200

    [BEBC-34] Added karma

---
+++ frontend/src/tests/exampleSpec.js
@@ -0,0 +1,12 @@
+/**
+ * Created by novy on 07.04.15.
+ */
+
+describe('Example test should ', function () {
+
+    it('always pass', function () {
+        expect(true).toBe(true);
+    });
+
+});
+
