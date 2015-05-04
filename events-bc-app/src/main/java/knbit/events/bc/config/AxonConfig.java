9c42ef4e119102bce26ff7fb7e04ac8df73eb905
novy154 <slnowak154@gmail.com>
2015-05-05 00:37:10 +0200

    [BEBC-102] Added axon configuration

---
+++ events-bc-app/src/main/java/knbit/events/bc/config/AxonConfig.java
@@ -0,0 +1,82 @@
+package knbit.events.bc.config;
+
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.SimpleCommandBus;
+import org.axonframework.commandhandling.annotation.AnnotationCommandHandlerBeanPostProcessor;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.axonframework.commandhandling.gateway.CommandGatewayFactoryBean;
+import org.axonframework.eventhandling.EventBus;
+import org.axonframework.eventhandling.SimpleEventBus;
+import org.axonframework.eventhandling.annotation.AnnotationEventListenerBeanPostProcessor;
+import org.axonframework.eventhandling.scheduling.EventScheduler;
+import org.axonframework.eventhandling.scheduling.java.SimpleEventSchedulerFactoryBean;
+import org.axonframework.eventstore.EventStore;
+import org.axonframework.eventstore.fs.EventFileResolver;
+import org.axonframework.eventstore.fs.FileSystemEventStore;
+import org.axonframework.eventstore.fs.SimpleEventFileResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.File;
+
+/**
+ * Created by novy on 04.05.15.
+ */
+
+@Configuration
+public class AxonConfig {
+
+    private static final String EVENTSTORE_FILEPATH = "data/eventstore";
+
+    @Bean
+    public EventBus eventBus() {
+        return new SimpleEventBus();
+    }
+
+    @Bean
+    public AnnotationEventListenerBeanPostProcessor annotationEventListenerBeanPostProcessor() {
+        AnnotationEventListenerBeanPostProcessor processor = new AnnotationEventListenerBeanPostProcessor();
+        processor.setEventBus(
+                eventBus()
+        );
+        return processor;
+    }
+
+    @Bean
+    public CommandBus commandBus() {
+        return new SimpleCommandBus();
+    }
+
+    @Bean
+    public AnnotationCommandHandlerBeanPostProcessor annotationCommandHandlerBeanPostProcessor() {
+        AnnotationCommandHandlerBeanPostProcessor processor = new AnnotationCommandHandlerBeanPostProcessor();
+        processor.setCommandBus(
+                commandBus()
+        );
+        return processor;
+    }
+
+    @Bean
+    public CommandGatewayFactoryBean<CommandGateway> commandGatewayFactoryBean() {
+        CommandGatewayFactoryBean<CommandGateway> factory = new CommandGatewayFactoryBean<>();
+        factory.setCommandBus(
+                commandBus()
+        );
+        return factory;
+    }
+
+    @Bean
+    public EventScheduler eventScheduler() throws Exception {
+        return new SimpleEventSchedulerFactoryBean().getObject();
+    }
+
+    @Bean
+    public EventStore eventStore() {
+        final File eventStoreFile = new File(EVENTSTORE_FILEPATH);
+        final EventFileResolver eventFileResolver = new SimpleEventFileResolver(eventStoreFile);
+        return new FileSystemEventStore(
+                eventFileResolver
+        );
+    }
+
+}
