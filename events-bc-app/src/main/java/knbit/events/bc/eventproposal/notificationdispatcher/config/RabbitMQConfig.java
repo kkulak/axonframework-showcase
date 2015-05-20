fa42fb355ac308a392df595c9a9f0622b53d6183
novy154 <slnowak154@gmail.com>
2015-05-20 13:10:08 +0200

    [BEBC-107] Fixed configuration files to use environment variables, fixed dockerbuild scripts

--- events-bc-app/src/main/java/knbit/events/bc/eventproposal/notificationdispatcher/config/RabbitMQConfig.java
+++ events-bc-app/src/main/java/knbit/events/bc/eventproposal/notificationdispatcher/config/RabbitMQConfig.java
@@ -4,13 +4,15 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Map;
 
 /**
  * Created by novy on 09.05.15.
@@ -22,8 +24,20 @@ public class RabbitMQConfig {
     public static final String QUEUE_NAME = "knbit-events-bc";
     private static final String TOPIC_EXCHANGE = "proposal-notification";
 
-    @Autowired
-    private ConnectionFactory connectionFactory;
+    private static final int RABBITMQ_SERVER_PORT = 5672;
+    private static final String RABBITMQ_ADDRESS_ENVIRONMENT_VARIABLE = "RABBITMQ_PORT_" + RABBITMQ_SERVER_PORT + "_TCP_ADDR";
+    private static final String DEFAULT_SERVER_IP = "127.0.0.1";
+
+    @Bean
+    ConnectionFactory connectionFactory() {
+        return new CachingConnectionFactory(determineSenderIpAddress(), 5672);
+    }
+
+    private String determineSenderIpAddress() {
+        final Map<String, String> environmentVariables = System.getenv();
+        return environmentVariables.getOrDefault(RABBITMQ_ADDRESS_ENVIRONMENT_VARIABLE, DEFAULT_SERVER_IP);
+    }
+
 
     @Bean
     RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
