a393c5118b44227092ec4d0ab18a64940bdafeec
Kamil Kulak <kulak.kam@gmail.com>
2015-05-22 13:53:03 +0200

    [BEBC-108] Added grouping by event state kanban read model

--- events-bc-app/src/main/java/knbit/events/bc/kanbanboard/readmodel/web/KanbanBoardController.java
+++ events-bc-app/src/main/java/knbit/events/bc/kanbanboard/readmodel/web/KanbanBoardController.java
@@ -1,11 +1,17 @@
 package knbit.events.bc.kanbanboard.readmodel.web;
 
+import knbit.events.bc.common.readmodel.EventStatus;
 import knbit.events.bc.kanbanboard.readmodel.model.KanbanBoard;
 import knbit.events.bc.kanbanboard.readmodel.repository.KanbanBoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @RestController
 @RequestMapping("/kanbanboard")
@@ -17,9 +23,11 @@ public class KanbanBoardController {
         this.kanbanBoardRepository = kanbanBoardRepository;
     }
 
-    @RequestMapping( method = RequestMethod.GET)
-    public Iterable<KanbanBoard> kanbanBoard() {
-        return kanbanBoardRepository.findAll();
+    @RequestMapping(method = RequestMethod.GET)
+    public Map<EventStatus, List<KanbanBoard>> kanbanBoard() {
+        return StreamSupport
+                .stream(kanbanBoardRepository.findAll().spliterator(), false)
+                .collect(Collectors.groupingBy(KanbanBoard::getEventStatus));
     }
 
 }
