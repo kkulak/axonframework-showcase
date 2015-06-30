bd3ec0deccfe462da4a35a9ad29cf3befc396716
novy154 <slnowak154@gmail.com>
2015-06-30 13:19:03 +0200

    [BEBC-119] Module init (those config classes should be later moved to a common module)

---
+++ member-questions-bc/src/main/java/knbit/memberquestions/bc/Application.java
@@ -0,0 +1,16 @@
+package knbit.memberquestions.bc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by novy on 30.06.15.
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
