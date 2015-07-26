f7a4109b25e5e9cf7e0d71ec9f9ca96614c048bc
novy154 <slnowak154@gmail.com>
2015-07-26 23:59:03 +0200

    [BEBC-138] configured interceptor

--- notification-bc/src/main/java/knbit/notification/bc/config/WebSocketConfig.java
+++ notification-bc/src/main/java/knbit/notification/bc/config/WebSocketConfig.java
@@ -1,5 +1,7 @@
 package knbit.notification.bc.config;
 
+import knbit.notification.bc.config.auth.WebSocketSecurityHandshakeInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
@@ -11,6 +13,9 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
     private static final String APP_PREFIX = "/notification-bc";
 
+    @Autowired
+    private WebSocketSecurityHandshakeInterceptor handshakeInterceptor;
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker(Topic.PREFIX);
@@ -19,7 +24,10 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint(Endpoint.INITIAL).setAllowedOrigins("*").withSockJS();
+//        registry.addEndpoint(Endpoint.INITIAL).setAllowedOrigins("*").withSockJS().setInterceptors(handshakeInterceptor);
+//        registry.addEndpoint(Endpoint.BATCH).setAllowedOrigins("*").withSockJS().setInterceptors(handshakeInterceptor);
+//        registry.addEndpoint(Endpoint.MESSAGE_STATE).setAllowedOrigins("*").withSockJS().setInterceptors(handshakeInterceptor);
+registry.addEndpoint(Endpoint.INITIAL).setAllowedOrigins("*").withSockJS();
         registry.addEndpoint(Endpoint.BATCH).setAllowedOrigins("*").withSockJS();
         registry.addEndpoint(Endpoint.MESSAGE_STATE).setAllowedOrigins("*").withSockJS();
     }
