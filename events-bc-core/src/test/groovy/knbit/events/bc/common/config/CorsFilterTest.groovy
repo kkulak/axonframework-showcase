61741b21c0179880faa73ed84cbc2aa51edf5901
novy154 <slnowak154@gmail.com>
2015-07-29 19:41:57 +0200

    [BEBC-139] Fixed CORSFilter so it accepts only subset of domains

---
+++ events-bc-core/src/test/groovy/knbit/events/bc/common/config/CorsFilterTest.groovy
@@ -0,0 +1,66 @@
+package knbit.events.bc.common.config
+
+import org.springframework.mock.web.MockHttpServletRequest
+import org.springframework.mock.web.MockHttpServletResponse
+import spock.lang.Specification
+
+import javax.servlet.FilterChain
+
+/**
+ * Created by novy on 29.07.15.
+ */
+class CorsFilterTest extends Specification {
+
+    def "given allowed domain with random port, it should set Access-Control-Allow-Origin as taken from request"() {
+        given:
+        def objectUnderTest = new CorsFilter()
+
+        when:
+        def request = new MockHttpServletRequest()
+        request.addHeader("Origin", "http://" + allowedDomain + ":6666")
+        def response = new MockHttpServletResponse()
+        objectUnderTest.doFilter(request, response, Mock(FilterChain))
+
+        then:
+        response.getHeader("Access-Control-Allow-Origin") == "http://" +allowedDomain + ":6666"
+
+        where:
+        allowedDomain << CorsFilter.ALLOWED_DOMAINS
+    }
+
+    def "if domain not allowed, it should set first allowed domain"() {
+        given:
+        def objectUnderTest = new CorsFilter()
+
+        when:
+        def request = new MockHttpServletRequest()
+        request.addHeader("Origin", "http://example.com")
+        def response = new MockHttpServletResponse()
+        objectUnderTest.doFilter(request, response, Mock(FilterChain))
+
+        then:
+        def firstAllowedDomain = CorsFilter.ALLOWED_DOMAINS.first()
+        response.getHeader("Access-Control-Allow-Origin") == "http://" + firstAllowedDomain
+
+        where:
+        allowedDomain << CorsFilter.ALLOWED_DOMAINS
+    }
+
+    def "given invalid url, it should set first allowed domain"() {
+        given:
+        def objectUnderTest = new CorsFilter()
+
+        when:
+        def request = new MockHttpServletRequest()
+        request.addHeader("Origin", "this is not fucking url")
+        def response = new MockHttpServletResponse()
+        objectUnderTest.doFilter(request, response, Mock(FilterChain))
+
+        then:
+        def firstAllowedDomain = CorsFilter.ALLOWED_DOMAINS.first()
+        response.getHeader("Access-Control-Allow-Origin") == "http://" + firstAllowedDomain
+
+        where:
+        allowedDomain << CorsFilter.ALLOWED_DOMAINS
+    }
+}
--- notification-bc/src/main/java/knbit/notification/bc/Application.java
+++ notification-bc/src/main/java/knbit/notification/bc/Application.java
@@ -1,13 +1,15 @@
 package knbit.notification.bc;
 
 import knbit.events.bc.auth.config.AAConfig;
+import knbit.events.bc.common.config.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import({
-        AAConfig.class
+        AAConfig.class,
+        CorsFilter.class
 })
 public class Application {
 
