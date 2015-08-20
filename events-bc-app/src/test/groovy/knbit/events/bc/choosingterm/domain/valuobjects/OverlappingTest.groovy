76b58030aac9031b29b6694f3f52249473e43451
novy154 <slnowak154@gmail.com>
2015-08-20 23:04:07 +0200

    [BEBC-160] jdk8 time -> jodatime

--- events-bc-app/src/test/groovy/knbit/events/bc/choosingterm/domain/valuobjects/OverlappingTest.groovy
+++ events-bc-app/src/test/groovy/knbit/events/bc/choosingterm/domain/valuobjects/OverlappingTest.groovy
@@ -1,9 +1,8 @@
 package knbit.events.bc.choosingterm.domain.valuobjects
 
+import org.joda.time.DateTime
+import org.joda.time.Duration
 import spock.lang.Specification
-
-import java.time.Duration
-import java.time.LocalDateTime
 
 /**
  * Created by novy on 19.08.15.
@@ -13,14 +12,14 @@ class OverlappingTest extends Specification {
     def "given another term with overlapping date and same location, should return true"() {
         when:
         def firstTermDuration = EventDuration.of(
-                LocalDateTime.of(2015, 1, 1, 18, 30), Duration.ofMinutes(90)
+                new DateTime(2015, 1, 1, 18, 30), Duration.standardMinutes(90)
         )
         def firstTerm = Term.of(
                 firstTermDuration, Capacity.of(120), Location.of("3.21C")
         )
 
         def secondTermDuration = EventDuration.of(
-                LocalDateTime.of(2015, 1, 1, 19, 0), Duration.ofMinutes(90)
+                new DateTime(2015, 1, 1, 19, 0), Duration.standardMinutes(90)
         )
         def secondTerm = Term.of(
                 secondTermDuration, Capacity.of(120), Location.of("3.21C")
@@ -33,14 +32,14 @@ class OverlappingTest extends Specification {
     def "given another term with overlapping date but different location, should return false"() {
         when:
         def firstTermDuration = EventDuration.of(
-                LocalDateTime.of(2015, 1, 1, 18, 30), Duration.ofMinutes(90)
+                new DateTime(2015, 1, 1, 18, 30), Duration.standardMinutes(90)
         )
         def firstTerm = Term.of(
                 firstTermDuration, Capacity.of(120), Location.of("3.21C")
         )
 
         def secondTermDuration = EventDuration.of(
-                LocalDateTime.of(2015, 1, 1, 19, 0), Duration.ofMinutes(90)
+                new DateTime(2015, 1, 1, 19, 0), Duration.standardMinutes(90)
         )
         def secondTerm = Term.of(
                 secondTermDuration, Capacity.of(120), Location.of("3.28C")
@@ -53,14 +52,14 @@ class OverlappingTest extends Specification {
     def "given another term with same location but not overlapping date, should return false"() {
         when:
         def firstTermDuration = EventDuration.of(
-                LocalDateTime.of(2015, 1, 1, 18, 30), Duration.ofMinutes(90)
+                new DateTime(2015, 1, 1, 18, 30), Duration.standardMinutes(90)
         )
         def firstTerm = Term.of(
                 firstTermDuration, Capacity.of(120), Location.of("3.21C")
         )
 
         def secondTermDuration = EventDuration.of(
-                LocalDateTime.of(2015, 1, 1, 20, 0), Duration.ofMinutes(90)
+                new DateTime(2015, 1, 1, 20, 0), Duration.standardMinutes(90)
         )
         def secondTerm = Term.of(
                 secondTermDuration, Capacity.of(120), Location.of("3.21C")
