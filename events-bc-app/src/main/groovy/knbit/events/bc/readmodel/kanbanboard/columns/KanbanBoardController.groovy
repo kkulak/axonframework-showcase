26936bbd65798e07e1392b2c0921203cb5ec3da2
Kamil Kulak <kulak.kam@gmail.com>
2015-09-27 15:30:50 +0200

    [BEBC-175] Moved kanban-board view to groovy & mongo

---
+++ events-bc-app/src/main/groovy/knbit/events/bc/readmodel/kanbanboard/columns/KanbanBoardController.groovy
@@ -0,0 +1,28 @@
+package knbit.events.bc.readmodel.kanbanboard.columns
+
+import com.mongodb.DBCollection
+import knbit.events.bc.auth.Authorized
+import knbit.events.bc.auth.Role
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping('/kanbanboard')
+class KanbanBoardController {
+    def DBCollection collection
+
+    @Autowired
+    KanbanBoardController(@Qualifier('kanban-board') DBCollection collection) {
+        this.collection = collection
+    }
+
+    @RequestMapping
+    @Authorized(Role.ADMIN)
+    def kanbanBoard() {
+        collection.find().toArray()
+            .groupBy { event -> event['eventStatus'] }
+    }
+
+}
---
+++ events-bc-app/src/main/groovy/knbit/events/bc/readmodel/kanbanboard/columns/KanbanBoardEventStatusHandler.groovy
@@ -0,0 +1,59 @@
+package knbit.events.bc.readmodel.kanbanboard.columns
+
+import knbit.events.bc.backlogevent.domain.valueobjects.events.BacklogEventEvents
+import knbit.events.bc.choosingterm.domain.valuobjects.events.UnderChoosingTermEventEvents
+import knbit.events.bc.interest.domain.valueobjects.events.InterestAwareEvents
+import org.axonframework.eventhandling.annotation.EventHandler
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Component
+
+import static knbit.events.bc.common.readmodel.EventStatus.*
+
+@Component
+class KanbanBoardEventStatusHandler {
+    def collection
+
+    @Autowired
+    KanbanBoardEventStatusHandler(@Qualifier("kanban-board") collection) {
+        this.collection = collection
+    }
+
+    @EventHandler
+    def on(BacklogEventEvents.Created event) {
+        def eventId = event.eventId()
+        def details = event.eventDetails()
+
+        collection.insert([
+                eventDomainId   : eventId.value(),
+                name            : details.name().value(),
+                eventType       : details.type(),
+                eventFrequency  : details.frequency(),
+                eventStatus     : BACKLOG,
+                reachableStatus : [BACKLOG, SURVEY_INTEREST, CHOOSING_TERM]
+        ])
+    }
+
+    @EventHandler
+    def on(InterestAwareEvents.Created event) {
+        collection.update(
+                [eventDomainId: event.eventId().value()],
+                [$set: [
+                        reachableStatus: [SURVEY_INTEREST, CHOOSING_TERM],
+                        eventStatus: SURVEY_INTEREST]
+                ]
+        )
+    }
+
+    @EventHandler
+    def on(UnderChoosingTermEventEvents.Created event) {
+        collection.update(
+                [eventDomainId: event.eventId().value()],
+                [$set: [
+                        reachableStatus: [CHOOSING_TERM],
+                        eventStatus: CHOOSING_TERM]
+                ]
+        )
+    }
+
+}
