dd7e3ec9cee41846346e09111dd1b791d3ffcfed
novy154 <slnowak154@gmail.com>
2015-10-11 11:52:44 +0200

    [BEBC-186] Enrollment event handler on member side

---
+++ events-bc-app/src/main/groovy/knbit/events/bc/readmodel/members/enrollment/handlers/EnrollmentMemberHandler.groovy
@@ -0,0 +1,58 @@
+package knbit.events.bc.readmodel.members.enrollment.handlers
+
+import com.mongodb.DBCollection
+import knbit.events.bc.choosingterm.domain.valuobjects.TermId
+import knbit.events.bc.common.domain.valueobjects.EventId
+import knbit.events.bc.enrollment.domain.valueobjects.MemberId
+import knbit.events.bc.enrollment.domain.valueobjects.events.EnrollmentEvents
+import org.axonframework.eventhandling.annotation.EventHandler
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Component
+
+/**
+ * Created by novy on 10.10.15.
+ */
+
+@Component
+class EnrollmentMemberHandler {
+
+    def DBCollection enrollmentEvents
+    def DBCollection enrollmentParticipants
+
+    @Autowired
+    EnrollmentMemberHandler(@Qualifier("enrollment-events") DBCollection enrollmentEvents,
+                            @Qualifier("enrollment-participants") DBCollection enrollmentParticipants) {
+        this.enrollmentEvents = enrollmentEvents
+        this.enrollmentParticipants = enrollmentParticipants
+    }
+
+    @EventHandler
+    def on(EnrollmentEvents.ParticipantEnrolledForTerm event) {
+        changeParticipantsCountBy(event.eventId(), event.termId(), 1)
+        saveMemberPreferences(event.eventId(), event.memberId(), event.termId())
+    }
+
+    private def saveMemberPreferences(EventId eventId, MemberId memberId, TermId termId) {
+        enrollmentParticipants.insert(
+                [eventId: eventId.value(), memberId: memberId.value(), termId: termId.value()]
+        )
+    }
+
+    @EventHandler
+    def on(EnrollmentEvents.ParticipantDisenrolledFromTerm event) {
+        changeParticipantsCountBy(event.eventId(), event.termId(), -1)
+        removeParticipantPreferences(event.eventId(), event.memberId())
+    }
+
+    private def removeParticipantPreferences(EventId eventId, MemberId memberId) {
+        enrollmentParticipants.remove([eventId: eventId.value(), memberId: memberId.value()])
+    }
+
+    def changeParticipantsCountBy(EventId eventId, TermId termId, int changeBy) {
+        enrollmentEvents.update(
+                [eventId: eventId.value(), 'terms.termId': termId.value()],
+                [$inc: ['terms.$.enrolled': changeBy]]
+        )
+    }
+}
