300e9a02bc2fc8fe7cc7f8c6208f8fc8341d7d1a
novy154 <slnowak154@gmail.com>
2015-10-25 19:58:33 +0100

    [BEBC-212] Created query for header

---
+++ events-bc-app/src/main/groovy/knbit/events/bc/readmodel/members/header/MembersHeaderQuery.groovy
@@ -0,0 +1,50 @@
+package knbit.events.bc.readmodel.members.header
+
+import com.mongodb.DBCollection
+import knbit.events.bc.enrollment.domain.valueobjects.MemberId
+import org.joda.time.DateTime
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.stereotype.Component
+
+/**
+ * Created by novy on 25.10.15.
+ */
+
+@Component
+class MembersHeaderQuery {
+
+    def DBCollection surveyCollection
+    def DBCollection dashboardEventsCollection
+
+    MembersHeaderQuery(@Qualifier("survey-events") DBCollection surveyCollection,
+                       @Qualifier("dashboard-events") DBCollection dashboardEventsCollection) {
+
+        this.surveyCollection = surveyCollection
+        this.dashboardEventsCollection = dashboardEventsCollection
+    }
+
+    def headerDataFor(MemberId memberId, DateTime date) {
+        surveyInProgress() + upcomingEvents(date) + nextEvent(memberId, date)
+    }
+
+    def newestHeaderData(MemberId memberId) {
+        headerDataFor(memberId, DateTime.now())
+    }
+
+    private def surveyInProgress() {
+        [surveyInProgress: surveyCollection.count()]
+    }
+
+    private def upcomingEvents(DateTime notEarlierThat) {
+        [upcomingEvents: dashboardEventsCollection.count([start: [$gte: notEarlierThat]])]
+    }
+
+    private def nextEvent(MemberId memberId, DateTime notEarlierThat) {
+        def nextEvent = dashboardEventsCollection.findOne([
+                attendees: memberId.value(),
+                start: [$gte: notEarlierThat]
+        ])
+
+        [nextEvent: nextEvent ?: []]
+    }
+}
